874faa20571597773177f53da3e1de05192ecb53
Vlad79X <109083165+Vlad79X@users.noreply.github.com>
2025-02-17 14:40:19 +0200

    RRAHH COPYRIGHT

--- centy_Library.py
+++ centy_Library.py
@@ -1,3 +1,29 @@
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Copyright (c) 2025 Nerdvana Centaurus
+# All rights reserved.
+#
+# Use of this software in programs that control a robot, with or without modification, is permitted  
+# provided that the following conditions are met:
+#
+# 1. Any program using this software must include the above copyright notice,  
+#    this list of conditions, and the following disclaimer in its documentation or source code.
+# 2. Any distributed software or firmware that incorporates this library must reproduce  
+#    the above copyright notice, this list of conditions, and the following disclaimer  
+#    in the documentation and/or other materials provided with the distribution.
+# 3. Neither the name of Nerdvana Centaurus nor the names of its contributors may be used  
+#    to endorse or promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,  
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, ARE DISCLAIMED. IN NO EVENT  
+# SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,  
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY  
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Enjoy coding with Pybricks and SPIKE!
+
 from pybricks.hubs import InventorHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
 from pybricks.parameters import Axis,Button, Color, Direction, Port, Side, Stop
